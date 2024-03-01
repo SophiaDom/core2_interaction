2d5210f43f6ea965f9d8186b026b2714ead28f70
Sophia Dombrowski <dombs723@newschool.edu>
2024-03-01 07:30:52 -0500

    finishing up

---
+++ p1/main/script.js
@@ -0,0 +1,4 @@
+document.getElementById('hideButton').addEventListener('click', function() {
+    var contentDiv = document.getElementsByClassName('.slide');
+    contentDiv.style.display = 'none'; // Hide the content
+});
